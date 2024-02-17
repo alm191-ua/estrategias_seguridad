44ae949923b30f7eca6d6ba065cbce238938aebd
Akira Llorens <78736401+alm191-ua@users.noreply.github.com>
2024-02-17 21:58:23 +0100

    unsafe decrypt, encryption menu

--- CrearZipYCodificar.py
+++ CrearZipYCodificar.py
@@ -13,12 +13,13 @@ import json
 
 TMP_DIR = 'tmp'
 # directorio de los ficheros
-AES_MODE = AES.MODE_EAX
+AES_MODE = AES.MODE_CBC
 
 FILE_DIR='files/'
 NAME_FILES='File'
 FILES_COMPRESSION_FORMAT='.zip'
-KEYS_FORMAT='.bin'
+CYPHER_FORMAT='.enc'
+KEYS_FORMAT='.bin' # TODO: Cambiar a .key
 UNSAFE_MODE = False
 UNSAFE_PASSWORDS = ['123456',
                     'admin',
@@ -32,6 +33,7 @@ UNSAFE_PASSWORDS = ['123456',
                     'qwerty',]
 KEY_SIZE = 16
 IV_SIZE = 16
+BLOCK_SIZE = 1024 # in bytes 
 DIRECTORIO=os.getcwd()
 
 
@@ -68,12 +70,12 @@ def Create_Dirs(filename):
     return directory
 
 
-def writeText(File,text):
+def writeText(File, text):
     with open(File, 'wb') as f:
         f.write(text)
     
 
-def ZipFile(files,title,description):
+def ZipFile(files, title, description):
     doc_Id = str(unique_id())
     FileName = NAME_FILES + str(doc_Id)
     directory = Create_Dirs(FileName)
@@ -113,7 +115,7 @@ def UnZipFile(file):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
       zip_ref.extractall(directory)
 
-def CreateJSON(directory,doc_id, title, description, files_names):
+def CreateJSON(directory, doc_id, title, description, files_names):
     # guardar el fichero .json
     logging.debug(f'Saving json file for document {doc_id}')
     doc_data = {
@@ -129,10 +131,10 @@ def CreateJSON(directory,doc_id, title, description, files_names):
 
     return json_filename
 
-def encrypt_file(input_file,directory):
+def encrypt_file(input_file, directory):
     key=generate_and_save_key(input_file,directory)
     iv = get_random_bytes(IV_SIZE) ##IMPORTANTE ESTO PORQUE SI NO NO FUNCIONA CBC
-    cipher = AES.new(key, AES.MODE_CBC, iv=iv) ##Se podría usar AES.MODE_ECB pero es poco seguro debido a que el bloque se cifra de la misma manera. CBC añade algo de alatoriedad
+    cipher = AES.new(key, AES_MODE, iv=iv) ##Se podría usar AES.MODE_ECB pero es poco seguro debido a que el bloque se cifra de la misma manera. CBC añade algo de alatoriedad
     path= os.path.join(directory, input_file + FILES_COMPRESSION_FORMAT)
     with open(path, 'rb') as f:
         plaintext = f.read()
@@ -141,33 +143,56 @@ def encrypt_file(input_file,directory):
     writeText(path,iv + ctext)
 
 
-def decrypt_file(input_file,directory):
-    key=read_key_from_file(input_file,directory)
+def decrypt_file(input_file, directory):
+    key=read_key_from_file(input_file, directory)
     path= os.path.join(directory, input_file + FILES_COMPRESSION_FORMAT)
     with open(path, 'rb') as f:
         iv = f.read(IV_SIZE)  # Read the first 16 bytes as the IV
         ciphertext = f.read()
-    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    cipher = AES.new(key, AES_MODE, iv=iv)
     padded_plaintext = cipher.decrypt(ciphertext)
    
     plaintext = unpad(padded_plaintext, AES.block_size)
     writeText(path,plaintext)
 
 
-def decrypt_file_unsafe(input_file,directory):
+def decrypt_file_unsafe(file_path, target_folder):
     for password in UNSAFE_PASSWORDS:
         key=bytes(password.ljust(KEY_SIZE, '0'), 'utf-8')
-        with open(directory+input_file+FILES_COMPRESSION_FORMAT, 'rb') as f:
+        ciphertext = b''
+
+        # file name removing extension
+        file_name = os.path.basename(file_path).split('.')[0]
+        # Read the encrypted file
+        with open(file_path, 'rb') as f:
             iv = f.read(IV_SIZE)  # Read the first 16 bytes as the IV
-            ciphertext = f.read()
-        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+            # ciphertext = f.read()
+            while True: # TODO: ¿Hay que leer el archivo por bloques para archivos grandes?
+                chunk = f.read(BLOCK_SIZE)
+                if len(chunk) == 0:
+                    break
+                ciphertext += chunk
+        # Decrypt the file
+        cipher = AES.new(key, AES_MODE, iv=iv)
         padded_plaintext = cipher.decrypt(ciphertext)
-    
+
+        # Remove the padding and save the file
         plaintext = unpad(padded_plaintext, AES.block_size)
-        writeText(directory+input_file+FILES_COMPRESSION_FORMAT,plaintext)
+        new_file = os.path.join(target_folder, file_name + FILES_COMPRESSION_FORMAT)
+        writeText(new_file, plaintext)
+
+        # Try to unzip the file
+        try:
+            with zipfile.ZipFile(new_file, 'r') as zip_ref:
+                zip_ref.extractall(target_folder)
+            logging.info(f'File decrypted with password: {password}')
+            return
+        except zipfile.BadZipFile:
+            logging.info(f'Password {password} is not valid')
+            os.remove(new_file)
 
 
-def generate_and_save_key(input_file,directory):
+def generate_and_save_key(input_file, directory):
     if(UNSAFE_MODE):
         key = bytes(random.choice(UNSAFE_PASSWORDS).ljust(KEY_SIZE, '0'), 'utf-8')
     else:
@@ -176,25 +201,58 @@ def generate_and_save_key(input_file,directory):
     return key
 
 
-def read_key_from_file(input_file,directory):
+def read_key_from_file(input_file, directory):
     path= os.path.join(directory, input_file + KEYS_FORMAT)
     with open(path, 'rb') as f:
         key = f.read()
     return key
 
-parser = argparse.ArgumentParser(description='Save documents in a secure way')
-parser.add_argument('-u', '--unsafe', help='Use unsafe mode', action='store_true')
-parser.add_argument('-d', '--decrypt', help='Start to decrypt a file (must be followed by File (-f))', action='store_true')
-parser.add_argument('-f', '--file', help='Indicates the file [put the directory eg: "python3 CrearZipYCodificar.py -d -f files/File2e47b658-6cdc-46ae-aa15-b3344bb3cbfd/]"')
-if(parser.parse_args().unsafe):
-    UNSAFE_MODE = True
-    logging.info('Unsafe mode activated')
-if(parser.parse_args().decrypt and parser.parse_args().file): ##Si se quiere desencriptar un archivo se comprueba que se haya indicado el archivo y se coge el nombre de la carpeta. Si no se llamará a ZipFile
-    match = re.search(r'files/(.*?)/', parser.parse_args().file)
-    file_name = match.group(1)
-    logging.info('Decrypt mode activated')
-    UnZipFile(file_name)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Save documents in a secure way')
+    parser.add_argument('-u', '--unsafe', help='Use unsafe mode', action='store_true')
+    parser.add_argument('-d', '--decrypt', help='Start to decrypt a file (must be followed by File (-f))', action='store_true')
+    parser.add_argument('-f', '--file', help='Indicates the file [put the directory eg: "python3 CrearZipYCodificar.py -d -f files/File2e47b658-6cdc-46ae-aa15-b3344bb3cbfd/]"')
+    if parser.parse_args().unsafe:
+        UNSAFE_MODE = True
+        logging.info('Unsafe mode activated')
+    if (parser.parse_args().decrypt and parser.parse_args().file): ##Si se quiere desencriptar un archivo se comprueba que se haya indicado el archivo y se coge el nombre de la carpeta. Si no se llamará a ZipFile
+        match = re.search(r'files/(.*?)/', parser.parse_args().file)
+        file_name = match.group(1)
+        logging.info('Decrypt mode activated')
+        UnZipFile(file_name)
 
+    if not parser.parse_args().decrypt:
+        logging.info('Encrypt mode activated')
+        
+        # Ask for the data and files to compress
+        title = input('Title: ')
+        description = input('Description: ')
+        same_folder = input('The files are in the same folder? (y/n): ')
+        if same_folder.lower() == 'y':
+            while True:
+                folder = input('Folder: ')
+                if not os.path.exists(folder):
+                    print('Folder does not exist')
+                    print('Select folder again')
+                else:
+                    break
+        files = []
+        while True:
+            file = input('File: ')
+            if same_folder.lower() == 'y':
+                file = os.path.join(folder, file)
+            if not os.path.exists(file):
+                print('File does not exist')
+                continue
+            files.append(file)
+            more_files = input('More files? (y/n): ')
+            if more_files.lower() != 'y':
+                break
+
+        # Compress and encrypt the files
+        ZipFile(files, title, description)
+
+        logging.info('Files encrypted')
 
 
 
