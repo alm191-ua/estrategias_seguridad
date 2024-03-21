7ac47db9e9b57e099d08ddabb1805e609b6ce202
Hugo Hernández López <value>
2024-03-21 14:31:56 +0100

    Refactorizado y con posibilidad de elegir opción

--- PruebaCliente.py
+++ PruebaCliente.py
@@ -3,6 +3,7 @@ import SocketCliente
 def main():
     cliente = SocketCliente.SocketCliente()
     cliente.connect()
+    cliente.send_int(1)
     cliente.send_files_in_folder()
     cliente.disconnect()
 if __name__ == "__main__":
